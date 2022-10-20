61129b8f36ffa02c8ceb554e55539256bf8fc6ec
Roope Vuorenlehto <rvuorenl@c1r3p6.hive.fi>
2022-10-20 20:45:21 +0300

    add unset_env to env.c. loops the env-list and copies, if match found, skip

--- srcs/env.c
+++ srcs/env.c
@@ -143,3 +143,47 @@ void	add_env(char *name, char *cont, char ***env, t_ms_help *h, size_t size)
 	*env = new_env;
 }
 
+void	copy_env_list_unset(char ***new, char ***env, char *name)
+{
+	int		i;
+	int		j;
+	size_t	len;
+
+	i = -1;
+	j = 0;
+	while ((*env)[++i])
+	{
+		len = get_longest_env_len(name, (*env)[i]);
+		if (ft_strncmp((const char *)(*env)[i], name, len) == 0)
+			continue ;
+		(*new)[j] = ft_strdup((*env)[i]);
+		if (!(*new)[j++])
+			exit(16);
+	}
+	(*new)[j] = NULL;
+}
+
+void	unset_env(char ***env, char *name, t_ms_help *help)
+{
+	char	*tmp;
+	char	**new;
+	int		i;
+	int		j;
+
+	i = -1;
+	j = 0;
+	tmp = get_env(name, *env, help);
+	if (ft_strlen(tmp) == 0)
+	{
+		ft_strdel(&tmp);
+		return ;
+	}
+	new = (char **)malloc(sizeof(char *) * help->env_size);
+	if (!new)
+		exit(15);
+	copy_env_list_unset(&new, env, name);
+	help->env_size--;
+	ft_free_doublearray(env);
+	*env = new;
+}
+
